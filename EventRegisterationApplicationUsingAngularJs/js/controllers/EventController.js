eec73d73830c5f54f50ab1a53bb2bf08838cd0ec
shabahat97 <shabahatalichishti@gmail.com>
2021-08-26 19:23:21 +0500

    handlingevents

--- EventRegisterationApplicationUsingAngularJs/js/controllers/EventController.js
+++ EventRegisterationApplicationUsingAngularJs/js/controllers/EventController.js
@@ -20,24 +20,36 @@ eventsApp.controller('EventController',
                     creatorName: 'Bob Smmith',
                     duration: '1 hr',
                     level: 'Advanced',
-                    abstract:'In this session you will learn the ins and outs of directives!'
+                    abstract: 'In this session you will learn the ins and outs of directives!',
+                    upVoteCount:0
                 },
                 {
                     name: 'Scopes for fun and profit',
                     creatorName: 'John Doe',
                     duration: '30 mins',
                     level: 'Introductory',
-                    abstract: 'In this session you will take a closer look at scopes'
+                    abstract: 'In this session you will take a closer look at scopes',
+                    upVoteCount: 0
+
                 },
                 {
                     name: 'Well Behaved Controller',
                     creatorName: 'Jane Doe',
                     duration: '2 hours',
                     level: 'Intermediate',
-                    abstract:'Controllers are the beginning of everything Angular does'
+                    abstract: 'Controllers are the beginning of everything Angular does',
+                    upVoteCount: 0
+
                 }
 
             ]
         }
+        $scope.upVoteSession = function (session) {
+            session.upVoteCount++;
+        };
+
+        $scope.downVoteSession = function (session) {
+            session.upVoteCount--;
+        };
     }
 );
